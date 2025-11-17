d93b09bbdeeaf7f84f23cafe3c311acdfe9daf8a
Satyam Kumar <iamsatyamsingh91@gmail.com>
2025-11-17 11:04:17 +0530

    update the backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -127,4 +127,41 @@ export const getUserCourseProgress = async(req,res)=>{
     catch(error){
         res.json({success: false , message: error.message})
     }
+} 
+
+
+export const addUserRating = async(req , res)=>{
+    const userId = req.auth.userId;
+    const {courseId , rating} = req.body;
+
+    if(!courseId || rating<1 || !userId || !rating || rating > 5){
+        return res.json({success: false, message : 'Invalid Details'});
+    }
+    try{
+        const course = await Course.findById(courseId);
+
+        if(!course)
+            return res.json({success: false , message: 'Course not found'});
+
+        const user = await User.findById(userId);
+
+        if(!user || !user.enrolledCourses.includes(courseId)){
+            return res.json({success: false , message:'User has not purchased this course'});
+        }
+
+        const existingRatingIndex = course.courseRatings.findIndex(r=>r.userId === userId)
+
+        if(existingRatingIndex > -1){
+            course.courseRatings[existingRatingIndex].rating = rating;
+        }
+        else{
+            course.courseRatings.push({userId , rating});
+        }
+
+        await course.save();
+        return res.json({success: true , message: 'Rating added'});
+
+    } catch(error){
+        return res.json({success: false , message: error.message});
+    }
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUserCourseProgress, getUserData , purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js'
+import { addUserRating, getUserCourseProgress, getUserData , purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js'
 
 import { requireAuth } from "@clerk/express";
 const userRouter = express.Router()
@@ -7,7 +7,8 @@ userRouter.use(requireAuth());
 userRouter.get('/data',getUserData)
 userRouter.get('/enrolled-courses' , userEnrolledCourses)
 userRouter.post('/purchase' , purchaseCourse)
+
 userRouter.post('/update-course-progress', updateUserCourseProgress)
 userRouter.post('/get-course-progress', getUserCourseProgress)
-
+userRouter.post('/add-rating', addUserRating)
 export default userRouter
