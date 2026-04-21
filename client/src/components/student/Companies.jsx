934ed0ce5c6a7c727edb4220551045c7122a2231
Satyam Kumar <iamsatyamsingh91@gmail.com>
2026-04-21 10:22:30 +0530

    update companies links

--- client/src/components/student/Companies.jsx
+++ client/src/components/student/Companies.jsx
@@ -16,9 +16,9 @@ export const Companies = () => {
       textColor: "text-blue-600",
       bgLight: "bg-blue-50",
       companies: [
-        { name: "Google", url: "https://www.naukri.com/code360/problems?company[]=Google", logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" },
-        { name: "Microsoft", url: "https://www.naukri.com/code360/problems?company[]=Microsoft", logo: "https://upload.wikimedia.org/wikipedia/commons/9/96/Microsoft_logo_%282012%29.svg" },
-        { name: "Amazon", url: "https://www.naukri.com/code360/problems?company[]=Amazon", logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" },
+        { name: "Google", url: "https://www.naukri.com/code360/problem-lists/top-google-coding-interview-questions", logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" },
+        { name: "Microsoft", url: "https://www.naukri.com/code360/problem-lists/top-microsoft-coding-interview-questions", logo: "https://upload.wikimedia.org/wikipedia/commons/9/96/Microsoft_logo_%282012%29.svg" },
+        { name: "Amazon", url: "https://www.naukri.com/code360/problem-lists/top-amazon-coding-interview-questions", logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" },
         { name: "Meta", url: "https://www.naukri.com/code360/problems?company[]=Facebook", logo: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Meta_Platforms_Inc._logo.svg" },
         { name: "Apple", url: "https://www.naukri.com/code360/problems?company[]=Apple", logo: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" },
         { name: "Netflix", url: "https://www.naukri.com/code360/problems?company[]=Netflix", logo: "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" },
